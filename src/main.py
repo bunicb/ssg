0755fc10d2dd9d670ed66c88a0a349ad64ffbfbf
Bojan Bunić <bojan@bunic.info>
2025-05-10 11:57:45 +0200

    textnode

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+def main():
+    # Create a TextNode object
+    node1 = TextNode("This is a link to example page", "link", "http://example.com")
+    
+    # Print the representation of the node
+    print(node1)
+
+main()
