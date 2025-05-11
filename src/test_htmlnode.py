0e6974048e29ffc587a91f57197a7b5027050a4c
Bojan Bunić <bojan@bunic.info>
2025-05-11 12:51:10 +0200

    leafnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props(self):
@@ -26,6 +26,28 @@ class TestHTMLNode(unittest.TestCase):
     def test_children_two_levels(self):
         node = HTMLNode("div", "This is a div", [HTMLNode("span", "This is a span", [HTMLNode("a", "This is a link")])], {"class": "test"})
         self.assertEqual(node.children[0].children[0].tag, "a")
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")  
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_leaf_to_html_no_tag_no_value(self):
+        node = LeafNode(None, None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+    
+    def test_leaf_to_html_with_props(self):
+        node = LeafNode("a", "This is a link", {"href": "http://example.org"})
+        self.assertEqual(node.to_html(), '<a href="http://example.org">This is a link</a>')
 
 if __name__ == "__main__":
     unittest.main()
